eaed70523032c859a19a104d86dd783c665fd871
张亮 <2286377528@qq.com>
2020-01-16 21:08:16 +0800

    05，完成头部组件封装的样式处理

--- components/header.vue
+++ components/header.vue
@@ -1,27 +1,74 @@
 <template>
   <div class="header">
-    <div class="logo">
-        <img src="/logo.jpg" alt="">
-    </div>
-     <div>
-         <nuxt-link to='/'>首页</nuxt-link>
-         <nuxt-link to='/post'>旅游攻略</nuxt-link>
-         <nuxt-link to='/hodel'>酒店</nuxt-link>
-         <nuxt-link to='/air'>国内机票</nuxt-link>
-     </div>
-   <div>
-       <span>消息</span>
-       <nuxt-link to='/user/login'></nuxt-link>
-   </div>
+    <el-row type="flex" class="row-bg" justify="space-between" align="middle">
+      <div class="logo">
+        <img src="/logo.jpg" alt />
+      </div>
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <div class="login">
+        <span>消息</span>
+        <nuxt-link to="/user/login">登录/注册</nuxt-link>
+      </div>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.header {
+  margin: 0 auto;
+  height: 60px;
+  line-height: 60px;
+  width: 1000px;
+  min-width: 1000px;
+  background: #fff;
+  border-bottom: 1px solid #eee;
+  box-shadow: 0, 3px, 3px, #ccc;
+  margin-bottom: 20px;
+}
+.row-bg {
+  // margin: 0,auto;
+  width: 1000px;
+  min-width: 1000px;
+  .logo img {
+    width: 156px;
+    height: 42px;
+    display: block;
+  }
+  .navs {
+    flex: 1;
+    a {
+      height: 57px;
+      display: block;
+      padding: 0 20px;
+      margin-left: 20px;
+      box-sizing: border-box;
+      &:hover {
+        color: #409eff;
+        border-bottom: 3px solid #409eff;
+        //    background-color: #409eff;
+      }
+    }
+    .nuxt-link-exact-active {
+      background-color: #409eff;
+      color: #fff;
+       &:hover{
+       color: #fff
+      }
+    }
+  }
+  .login {
+    span {
+      margin-right: 30px;
+    }
+  }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- 这是首页文件 默认路径为 / -->
   <div class="index">
-    首页
+  
   </div>
 </template>
 
