f6b9bb0ee1039ac4a6d5069a01ab0af283e05348
张亮 <2286377528@qq.com>
2020-01-16 20:21:36 +0800

    04，封装头部和页脚公共组件，并引入根组件中。

---
+++ components/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <div class="logo">
+        <img src="/logo.jpg" alt="">
+    </div>
+     <div>
+         <nuxt-link to='/'>首页</nuxt-link>
+         <nuxt-link to='/post'>旅游攻略</nuxt-link>
+         <nuxt-link to='/hodel'>酒店</nuxt-link>
+         <nuxt-link to='/air'>国内机票</nuxt-link>
+     </div>
+   <div>
+       <span>消息</span>
+       <nuxt-link to='/user/login'></nuxt-link>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,49 @@
 <template>
 <!-- 这是根组件 -->
   <div class="def">
+    <!-- 头部公共组件 -->
+    <Header></Header>
     <!-- 相当于<router-view></router-view> 组件映射 -->
     <nuxt />
- 
+   <!-- 页脚公共组件 -->
+   <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 export default {
-
+components: {
+  Header,Footer
+}
 }
 </script>
 
-<style>
-
+<style lang='less'>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+ *{
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box
+ }
+ a {
+   text-decoration: none;
+   color: #ccc;
+   }
+ em,i {
+   font-style: normal
+ }
+ ul,li,ol{
+   list-style: none
+ }
 </style>
