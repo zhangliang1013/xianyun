0ab4167e9146349147348d95d3eda2f4e55a5588
张亮 <2286377528@qq.com>
2020-02-14 17:40:46 +0800

    32,分页组件添加成功

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,88 +1,108 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                 <FlightsListHead></FlightsListHead>
-                
-                
-                <!-- 航班信息 -->
-                 <FlightsItem v-for="(item,index) in flightsData.flights" :key="index" :data='item'></FlightsItem>
-            </div>
+        <!-- 航班头部布局 -->
+        <FlightsListHead></FlightsListHead>
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 航班信息 -->
+        <FlightsItem v-for="(item,index) in flightsData.flights" :key="index" :data="item"></FlightsItem>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+
+    <!-- 分页组件 -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageIndex"
+      :page-sizes="[3, 5, 8, 10]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    ></el-pagination>
+  </section>
 </template>
 
 <script>
-import FlightsListHead from '@/components/air/flightsListHead';
-import FlightsItem from '@/components/air/flightsItem'
+import FlightsListHead from "@/components/air/flightsListHead";
+import FlightsItem from "@/components/air/flightsItem";
 
 export default {
-    components : {
-    FlightsListHead,FlightsItem
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  data() {
+    return {
+      // 储存参数
+      getParameter: {
+        departCity: "",
+        departCode: "",
+        destCity: "",
+        destCode: "",
+        departDate: ""
+      },
+      // 储存机票列表信息
+      flightsData: {},
+      // 备份机票列表信息
+      backupFlightsData: {},
+      //   分页的数据
+      pageIndex: 3,
+      pageSize: 1
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(val);
     },
-    data(){
-        return {
-            // 储存参数
-            getParameter : {
-                departCity : '',
-                departCode : '',
-                destCity : '',
-                destCode : '',
-                departDate : ''
-            },
-            // 储存机票列表信息
-            flightsData : {},
-            // 备份机票列表信息
-            backupFlightsData : {}
-        }
-    },
-    mounted(){
-        // 获取机票列表信息
-        const data = this.$route.query;
-        const {goCity , goCityNum, getCity ,getCityNum,goDate} = data;
-        this.getParameter.departCity = goCity
-        this.getParameter.departCode = goCityNum
-        this.getParameter.destCity =getCity
-        this.getParameter.destCode = getCityNum
-        this.getParameter.departDate = goDate
-        this.$axios({
-            url : '/airs',
-            params : this.getParameter
-        }).then(res => {
-            // console.log(res)
-            this.flightsData = res.data;
-            console.log(this.flightsData)
-            this.backupFlightsData = {...res.data};
-        })
-    }
-}
+    handleCurrentChange(val) {}
+  },
+  mounted() {
+    // 获取机票列表信息
+    const data = this.$route.query;
+    const { goCity, goCityNum, getCity, getCityNum, goDate } = data;
+    this.getParameter.departCity = goCity;
+    this.getParameter.departCode = goCityNum;
+    this.getParameter.destCity = getCity;
+    this.getParameter.destCode = getCityNum;
+    this.getParameter.departDate = goDate;
+    this.$axios({
+      url: "/airs",
+      params: this.getParameter
+    }).then(res => {
+      // console.log(res)
+      this.flightsData = res.data;
+      console.log(this.flightsData);
+      this.backupFlightsData = { ...res.data };
+    });
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
+.el-pagination {
+    text-align: center;
+}
 </style>
